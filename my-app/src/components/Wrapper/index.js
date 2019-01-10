c5b41ca3e17569531c3fbcad4e659d2f39043e50
tyty1602 <37279775+tyty1602@users.noreply.github.com>
2019-01-09 18:45:08 -0800

    clicky game

---
+++ my-app/src/components/Wrapper/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Wrapper(props) {
+  return <div className="wrapper">{props.children}</div>;
+}
+
+//render return div ,...
+
+
+export default Wrapper;
