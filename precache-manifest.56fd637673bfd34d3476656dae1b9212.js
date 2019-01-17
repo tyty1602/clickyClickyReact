fd99f7afdb93f82bcc3dd08247b937dd8314e20d
tyty1602 <37279775+tyty1602@users.noreply.github.com>
2019-01-16 18:15:32 -0800

    Updates

---
+++ precache-manifest.56fd637673bfd34d3476656dae1b9212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a80ae7ce3f47ac021552",
+    "url": "/clickyClickyReact/static/js/runtime~main.a80ae7ce.js"
+  },
+  {
+    "revision": "25ed28ae27d4e1a4bfa2",
+    "url": "/clickyClickyReact/static/js/main.25ed28ae.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clickyClickyReact/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "25ed28ae27d4e1a4bfa2",
+    "url": "/clickyClickyReact/static/css/main.fe038655.chunk.css"
+  },
+  {
+    "revision": "6d9e93ad16bf59e753c572f49012706e",
+    "url": "/clickyClickyReact/index.html"
+  }
+];
